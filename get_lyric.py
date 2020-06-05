a2eaaa791ebcde31367e03e20b984de24609ffb0
lukaszsulek <66498057+lukaszsulek@users.noreply.github.com>
2020-06-05 15:46:49 -0700

    Create get_lyric.py

---
+++ get_lyric.py
@@ -0,0 +1,25 @@
+# Written by Lukasz Sulek
+#
+# Credit goes to John W. Miller for publishing the lyricsgenius module available below:
+# https://github.com/johnwmillr/LyricsGenius
+
+import lyricsgenius
+import sys
+
+# Create a free genius.com account to set up API access and replace/paste the key below
+genius = lyricsgenius.Genius("API_KEY")
+
+# Turn off status messages
+genius.verbose = False
+
+# Script is invoked like so when called by the external_command method:
+#     [0]                [1]      [2]       [3]    [4]
+#     \path\get_lyric.py --artist "$ARTIST" --song "$SONG"
+song = genius.search_song(sys.argv[4], sys.argv[2])
+
+# Note: if running on Windows, you may get a UnicodeEncodeError exception due to, well, Windows/Python default encoding
+# Follow the steps here http://www.tiernok.com/posts/python-3-6-unicode-output-for-windows-console.html
+# This will tell your python3 environment to use UTF-8
+#   PYTHONIOENCODING UTF-8
+
+print(song.lyrics)
